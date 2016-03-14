c87bd7feebd7845543023e7404b2250a51155223
Don Kelly <karfai@gmail.com>
2016-03-13 23:09:04 -0400

    Send rules to the repository

--- Gemfile
+++ Gemfile
@@ -2,6 +2,8 @@ source 'https://rubygems.org'
 
 ruby "2.2.4"
 
+gem 'faraday'
+gem 'faraday_middleware'
 gem 'grape'
 gem 'grape-middleware-logger'
 gem 'mongoid', '~> 5.1.0'
--- populate.rb
+++ populate.rb
@@ -1,5 +1,10 @@
+require 'faraday'
+require 'faraday_middleware'
 require 'multi_json'
+
 require_relative './models'
+
+changes = []
 
 Dir['./*.json'].each do |fn|
   name = File.basename(fn, '.json')
@@ -10,10 +15,25 @@ Dir['./*.json'].each do |fn|
   if rule && rule.version != ver
     puts "# updating"
     rule.update_attributes(attrs)
+    changes << rule
   elsif !rule
     puts "# creating"
-    Rule.create(attrs)
+    changes << Rule.create(attrs)
   else
     puts "# nothing"
   end
+end
+
+changes.each do |rule|
+  url = ENV.fetch('TATEV_REGISTRY_URL', 'http://localhost:3000')
+  conn = Faraday.new(url) do |f|
+    f.request(:url_encoded)
+    f.request(:json)
+    f.response(:json, :content_type => /\bjson$/)
+    f.adapter(Faraday.default_adapter)        
+  end
+
+  puts "> #{url}/api/v1/rules/#{rule.name}/#{rule.version}"
+  resp = conn.put("/api/v1/rules/#{rule.name}", rule: { version: rule.version })
+  puts "< #{resp.status}"
 end
